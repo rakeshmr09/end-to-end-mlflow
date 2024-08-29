39a58ea6c8b08c271eaca5d92dca9a2583df8812
rakesh 09 <rakeshmr9999@gmail.com>
2024-08-29 16:35:02 +0530

    final

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTraining
 from src.mlProject.pipeline.stage_02_data_validation import DataValidationPipeline 
 from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationPipeline 
 from src.mlProject.pipeline.stage_04_model_trainer import ModelTrainerPipeline 
+from src.mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationPipeline
 
 logger.info("Welcome to custom logging")
 
@@ -50,6 +51,19 @@ try:
     logger.info(f"stage {STAGE_NAME} started" )
     model_trainer = ModelTrainerPipeline()
     model_trainer.main()
+    logger.info(f"stage {STAGE_NAME} completed" )
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Model Evaluation Started"
+
+try:
+    logger.info(f"stage {STAGE_NAME} started" )
+    model_evaluation = ModelEvaluationPipeline()
+    model_evaluation.main()
     logger.info(f"stage {STAGE_NAME} completed" )
 except Exception as e:
     logger.exception(e)
